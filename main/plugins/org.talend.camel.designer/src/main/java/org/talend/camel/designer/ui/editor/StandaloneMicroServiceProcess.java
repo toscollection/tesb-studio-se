eeec9de652c9ddae07253f77608f892cddcdd7ca
Abdulhamid Rashankar <arashankar@talend.com>
2023-11-12 10:09:39 +0100

    APPINT-36008: update code generation for standlone microservice

---
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/editor/StandaloneMicroServiceProcess.java
@@ -0,0 +1,22 @@
+package org.talend.camel.designer.ui.editor;
+
+import java.util.Map;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.talend.core.model.properties.Property;
+import org.talend.core.runtime.process.LastGenerationInfo;
+
+public class StandaloneMicroServiceProcess  extends RouteProcess {
+
+    public StandaloneMicroServiceProcess(Property property) {
+        super(property);
+    }
+
+    public boolean isEnableMetrics() {
+        Map<String, Object> argumentsMap = LastGenerationInfo.getInstance().getLastMainJob().getProcessor().getArguments();
+
+        return ObjectUtils.equals(argumentsMap.get("ESB_METRICS"), new Boolean(true));
+
+    }
+
+}
